8fb8c837467b8bd846d3ea19996092ee4f136c07
Carlos Eduardo Arantes Ferreira <carantes@mobistart.com.br>
2015-08-29 19:31:37 -0300

    change app port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(8081, function() {
+var server = app.listen(18320, function() {
 	var port = server.address().port;
     console.log('Heroku Server now running at http://localhost:', port);
 });
