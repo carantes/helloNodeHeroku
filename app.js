2f9417273cff451b97dc67847f6160f24c39c075
Carlos Eduardo Arantes Ferreira <carantes@mobistart.com.br>
2015-08-29 17:43:34 -0300

    Ajustes

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var 	express = require('express'),
 
 var server = app.listen(8081, function() {
 	var port = server.address().port;
-    console.log('Heroku Server running at http://localhost:', port);
+    console.log('Heroku Server now running at http://localhost:', port);
 });
