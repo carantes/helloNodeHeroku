1d681fd06a8726bdc4be8e375e48a452c4fad115
Carlos Eduardo Arantes Ferreira <carantes@mobistart.com.br>
2015-08-29 19:41:21 -0300

    listen to default heroku port or 5000

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(18320, function() {
+var server = app.listen(process.env.PORT || 5000, function() {
 	var port = server.address().port;
     console.log('Heroku Server now running at http://localhost:', port);
 });
