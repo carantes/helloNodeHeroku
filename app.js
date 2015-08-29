cc7eeaa5e323c99f5cffbf1b7a803ff45e23c029
Carlos Eduardo Arantes Ferreira <carantes@mobistart.com.br>
2015-08-29 17:48:16 -0300

    Add basic route to hello world!

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var 	express = require('express'),
   		app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
 var server = app.listen(8081, function() {
 	var port = server.address().port;
     console.log('Heroku Server now running at http://localhost:', port);
